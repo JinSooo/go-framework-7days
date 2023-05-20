0a8d0a36a161b491c3b75c517db0658916cdc5c0
JinSo <kimjinso@qq.com>
2023-05-20 14:05:32 +0800

    style(GeeCache): 🎨 修改geecache的注释

--- GeeCache/geecache/byteview.go
+++ GeeCache/geecache/byteview.go
@@ -2,6 +2,8 @@ package geecache
 
 /* -------------------------------- 缓存值的抽象与封装 ------------------------------- */
 /**
+ * 实现 Value 接口
+ *
  * ByteView 只有一个数据成员，b []byte，b 将会存储真实的缓存值。
  * 选择 byte 类型是为了能够支持任意的数据类型的存储
  */
@@ -20,7 +22,7 @@ func (byteView ByteView) String() string {
 	return string(byteView.b)
 }
 
-// 复制一个数据副本
+// 使用 ByteSlice() 方法返回一个拷贝，防止缓存值被外部程序修改
 func (byteView ByteView) ByteSlice() []byte {
 	return cloneBytes(byteView.b)
 }
