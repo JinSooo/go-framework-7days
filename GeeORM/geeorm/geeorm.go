3ac6f731e0cfb7da173160cf467a01906cd3dc1b
JinSo <kimjinso@qq.com>
2023-05-25 17:10:53 +0800

    feat(GeeORM): ✨ 数据库迁移
    - 结构体(struct)变更时，数据库表的字段(field)自动迁移(migrate)
    - 仅支持字段新增与删除，不支持字段类型变更

--- GeeORM/geeorm/geeorm.go
+++ GeeORM/geeorm/geeorm.go
@@ -2,9 +2,11 @@ package geeorm
 
 import (
 	"database/sql"
+	"fmt"
 	"geeorm/dialect"
 	"geeorm/log"
 	"geeorm/session"
+	"strings"
 )
 
 /* ---------------------------------- 与用户交互 --------------------------------- */
@@ -84,4 +86,77 @@ func (engine *Engine) Transaction(fn TxFunc) (result interface{}, err error) {
 	}()
 
 	return fn(session)
+}
+
+/* --------------------------------- Migrate -------------------------------- */
+/**
+ * 新增字段
+ * 		ALTER TABLE table_name ADD COLUMN col_name col_type;
+ *
+ * 删除字段
+ * 		CREATE TABLE new_table AS SELECT col1, col2, ... from old_table
+ * 		DROP TABLE old_table
+ * 		ALTER TABLE new_table RENAME TO old_table;
+ */
+
+// a - b
+func difference(a []string, b []string) (diff []string) {
+	m := make(map[string]bool)
+	for _, v := range b {
+		m[v] = true
+	}
+
+	for _, v := range a {
+		if _, ok := m[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+
+	return
+}
+
+// 表迁移
+func (engine *Engine) Migrate(value interface{}) error {
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		// 注意：这边 s.Model(value) 的时候，refTable已经更新成新的Model了
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesn't exist", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+
+		// 新的Model
+		table := s.RefTable()
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s", table.Name)).QueryRows()
+		// 老的Model
+		columns, _ := rows.Columns()
+
+		// 需要添加和删除的column
+		addCols := difference(table.FiledNames, columns)
+		delCols := difference(columns, table.FiledNames)
+		log.Infof("added cols: %v, deleted cols: %v", addCols, delCols)
+
+		// 新增字段
+		for _, col := range addCols {
+			field := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;", table.Name, field.Name, field.Type)
+			if _, err = s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+
+		// 删除字段
+		if len(delCols) == 0 {
+			return
+		}
+
+		tmpTable := "tmp_" + table.Name
+		fieldStr := strings.Join(table.FiledNames, ", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s from %s;", tmpTable, fieldStr, table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s;", tmpTable, table.Name))
+		_, err = s.Exec()
+		return
+	})
+
+	return err
 }
